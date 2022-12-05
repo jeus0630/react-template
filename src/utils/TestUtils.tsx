eeae7c2fbafea78fb41be9a55f85f132b2da6ea5
jewoo <jeus0630@gmail.com>
2022-12-05 22:13:56 +0900

    Configure recoil for test code

--- src/utils/TestUtils.tsx
+++ src/utils/TestUtils.tsx
@@ -2,11 +2,12 @@
 import React, { FC, ReactElement } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
 import { QueryClientProvider, QueryClient } from 'react-query';
+import { RecoilRoot } from 'recoil';
 
 const AllTheProviders: FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <QueryClientProvider client={new QueryClient()}>
-      {children}
+      <RecoilRoot>{children}</RecoilRoot>
     </QueryClientProvider>
   );
 };
